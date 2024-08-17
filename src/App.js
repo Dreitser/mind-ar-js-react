e596d8bb67ad2c0f1931e5b03380955cd4e23500
Lex Dreitser <Dreitser@users.noreply.github.com>
2024-08-16 20:27:03 -0700

    1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [started, setStarted] = useState(null);
 
   return (
-    <div className="h-screen ">
+    <div className="h-screen bg-red-700">
       <h1>Example React component with AR </h1>
 
       <div className="control-buttons">
@@ -16,7 +16,7 @@ function App() {
       </div>
 
 
-        <div className="h-screen absolute inset-0 " >
+        <div className="h-screen absolute inset-0 bg-red-700 " >
           < MindARViewer className="h-screen " />
           <video></video>
         </div>
